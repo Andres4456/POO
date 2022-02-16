26f8bcb40279c6939650c2fa7bda2ccf8f8bb8b3
Andres4456 <aluque0934@ieselcaminas.org>
2022-02-16 18:18:12 +0100

    Initial commit

---
+++ src/Cuenta.java
@@ -0,0 +1,10 @@
+public class Cuenta {
+    private String nombreDelCliente;
+    public String getNombreDelCliente() {return nombreDelCliente;}
+
+    private String numeroDeCuenta;
+    public String getNumeroDeCuenta() {return numeroDeCuenta;}
+
+    private String Saldo;
+    public String getSaldo() {return Saldo;}
+}
